a1e0467ebc3f477c1c8690d7db22817fbae28013
nirdun <nirdun@gmail.com>
2016-12-27 20:21:16 +0200

    json start

--- src/main/java/bgu/spl/a2/sim/Simulator.java
+++ src/main/java/bgu/spl/a2/sim/Simulator.java
@@ -5,9 +5,17 @@
  */
 package bgu.spl.a2.sim;
 
-import bgu.spl.a2.WorkStealingThreadPool;
+import bgu.spl.a2.*;
+import bgu.spl.a2.sim.conf.ManufactoringPlan;
+import bgu.spl.a2.sim.tools.GcdScrewDriver;
+import bgu.spl.a2.sim.tools.NextPrimeHammer;
+import bgu.spl.a2.sim.tools.RandomSumPliers;
 import com.google.gson.Gson;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
@@ -15,12 +23,68 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public class Simulator {
     static WorkStealingThreadPool threadPool;
+    private int numOfThreds;
+    private List<JsonTools> jTools;
+    private List<JsonPlans> jPlans;
+    private List<List<JsonWaves>> jWaves;
+    private Warehouse wareHouse;
+
+    public Simulator() {
+        jPlans = new LinkedList<>();
+        jTools = new LinkedList<>();
+        jWaves = new LinkedList<>();
+        wareHouse=new Warehouse();
+
+    }
+
 
     /**
      * Begin the simulation
      * Should not be called before attachWorkStealingThreadPool()
      */
     public static ConcurrentLinkedQueue<Product> start() {
+        ConcurrentLinkedQueue<Product> manufactoredProducts = new ConcurrentLinkedQueue<>();
+
+    }
+
+    public void readFromJson(String[] args) {
+        Gson gson = new Gson();
+        String file = args[0];
+        try (FileReader fileReader = new FileReader(file);) {
+            JsonRes jasonRes = gson.fromJson(fileReader, JsonRes.class);
+            numOfThreds = jasonRes.getThreads();
+            jTools=jasonRes.getTools();
+            jPlans=jasonRes.getPlans();
+            jWaves=jasonRes.getWaves();
+
+            //Todo function to switch case
+
+            for(JsonTools jtool:jTools) {
+                switch (jtool.getTool()) {
+                    case "gs-driver":
+                        wareHouse.addTool(new GcdScrewDriver(),jtool.getQty());
+                        break;
+                    case "np-hammer":
+                        wareHouse.addTool(new NextPrimeHammer(),jtool.getQty());
+                        break;
+                    case "rs-pliers":
+                        wareHouse.addTool(new RandomSumPliers(),jtool.getQty());
+                        break;
+                }
+            }
+
+            for(JsonPlans jPlan:jPlans){
+                String newProduct=jPlan.getProduct();
+                String newParts[]=jPlan.getParts().toArray(new String[jPlan.getParts().size()]);
+                String newTools[]=jPlan.getTools().toArray(new String[jPlan.getTools().size()]);
+                wareHouse.addPlan(new ManufactoringPlan(newProduct,newParts,newTools));
+            }
+
+
+        } catch (IOException ioExc) {
+
+        }
+
     }
 
     /**
@@ -33,6 +97,5 @@ public class Simulator {
     }
 
     public static int main(String[] args) {
-        Gson gson = new Gson();
     }
 }
